d0b6c76eb48c749c8e73ca92b14b9f79bd4bc3d9
Jeck0-0 <48527796+Jeck0-0@users.noreply.github.com>
2022-04-03 01:15:03 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  stoat
 // @author       oralekin (stoatified by Jeck0_0)
+// @match        https://hot-potato.reddit.com/embed*
 // @icon         https://cdn.discordapp.com/attachments/413985144219893760/959922796966854706/Stoat.png
 // @grant        none
 // ==/UserScript==
